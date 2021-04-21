4f92bad7bd9a2b54a94ba2c983d14e5ba44c019b
Gean Kaminski <geankaminski93@hotmail.com>
2021-04-21 08:50:44 -0300

    migration for users

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,13 @@
 import { Router } from "express";
 import { SettingsController } from "./controllers/SettingsController";
-
+import { UsersController } from "./controllers/UsersController";
 
 const routes = Router();
 
 const settingsController = new SettingsController();
-
-//Routes params => localhost:3333/settings/1
-//Query params => localhost:3333/settings/1?search=a
-//Body params => { na request }
+const usersController = new UsersController();
 
 routes.post("/settings", settingsController.create)
+routes.post("/users", usersController.create)
 
 export { routes };
